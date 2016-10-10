890dffaae389485b6e775281d4a2322f69409744
Claude Andrianarijaona <candrianarijaona@infopro-digital.com>
2016-10-11 00:54:33 +0200

    [MISC] adding composer.lock in gitignore

---
+++ src/Breadcrumb.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samue
+ * Date: 11/10/2016
+ * Time: 00:14
+ */
+
+namespace Breadcrumb;
+
+
+class Breadcrumb
+{
+
+}
---
+++ src/BreadcrumbItem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samue
+ * Date: 11/10/2016
+ * Time: 00:18
+ */
+
+namespace Breadcrumb;
+
+
+class BreadcrumbItem
+{
+
+}
